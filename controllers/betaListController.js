ad1e59467976660b0ff4833c204f7dcfccbf31ce
OmriNach <omnachmani@gmail.com>
2023-10-14 16:24:04 -0400

    Update betaListController.js

--- controllers/betaListController.js
+++ controllers/betaListController.js
@@ -280,6 +280,6 @@ exports.emailInviteToUsers = async (req, res) => {
 
     }
 
-    res.status(200).send('Emails sent successfully', success_status_per_email);
+    res.status(200).send(success_status_per_email);
 }
 
