85c441a215c7d44cfb5a250e91f818fd40041d33
OmriNach <omnachmani@gmail.com>
2023-10-25 14:15:09 -0400

    Update sourceController.js

--- controllers/sourceController.js
+++ controllers/sourceController.js
@@ -2,7 +2,7 @@
 const axios = require('axios');
 
 const Source = require("../models/Source");
-const PIPELINE_API_URL = process.env.PIPELINE_API_URL || "http://15.222.245.200:5000";
+const PIPELINE_API_URL = process.env.PIPELINE_API_URL || "http://3.96.166.225:5000";
 exports.store = async (req, res) => {
   try {
     const sourceData = req.body;
