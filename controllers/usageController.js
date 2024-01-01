306a854ae3398277649b437a37bd61074c55085c
OmriNach <omnachmani@gmail.com>
2024-01-01 18:39:42 -0500

    api usage and logs sending to frontend

--- controllers/usageController.js
+++ controllers/usageController.js
@@ -2,22 +2,28 @@ const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 const UsageEntry = require("../models/UsageEntry");
 const APICustomer = require("../models/APICustomer");
-
+const BackendChatLog = require("../models/BackendChatLog");
 
 exports.index = async (req, res) => {
     try {
         console.log('fetching usage entries');
         const page = parseInt(req.query.page) || 1; // Get the requested page or default to page 1
-        const perPage = parseInt(req.query.perPage) || 10; // Get the requested number of items per page or default to 10
+        const perPage = parseInt(req.query.perPage) || 50; // Get the requested number of items per page or default to 10
         const skip = (page - 1) * perPage;
+        const customer_key = req.query.customer || "";
 
+        const query = {};
+        if (customer_key) {
+            query.api_key = customer_key;
+        }
         //get all api customers
         const apiCustomers = await APICustomer.find({});
         //get all usage entries
-        const usageEntries = await UsageEntry.find({}).sort({ timestamp: -1 }).skip(skip).limit(perPage);
-
+        const usageEntries = await UsageEntry.find(query).sort({ timestamp: -1 }).skip(skip).limit(perPage);
+        const chatLogs = await BackendChatLog.find(query).sort({ timestamp: -1 }).skip(skip).limit(perPage);
         //return api customers and usage entries
-        res.status(200).json({ apiCustomers, usageEntries });
+        const data = { apiCustomers, usageEntries, chatLogs };
+        res.status(200).json(data);
     }
     catch (error) {
         console.log('error', error);
---
+++ models/BackendChatLog.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+pendium_db = mongoose.createConnection(process.env.MONGO_URL_2, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const BackendChatLogSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    datetime: Date,
+    question: String,
+    answer: String,
+    chat_history: [[String]],
+    filters: {
+      source_type: {
+        $in: [String]
+      }
+    },
+    api_key: String,
+    response_mode: String
+  });
+  
+  module.exports = pendium_db.model('BackendChatLog', BackendChatLogSchema, 'chat_logs');
+  
