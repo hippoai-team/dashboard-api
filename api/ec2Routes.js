61d197763a44327123b86a1bafedf2e110d27d79
OmriNach <omnachmani@gmail.com>
2024-06-20 21:59:50 -0400

    checks for instance and docker starts

--- api/ec2Routes.js
+++ api/ec2Routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ec2Controller = require('../controllers/ec2Controller');
 
-router.post('/launch', ec2Controller.start);
+router.post('/start', ec2Controller.start);
 router.post('/stop', ec2Controller.stop);
 router.get('/check-state', ec2Controller.check_instance_state);
 
